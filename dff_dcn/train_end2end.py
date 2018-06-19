288008f42d8d2f63051e53e50e9d41bec57a8697
Jiarui XU <xvjiarui0826@gmail.com>
2018-06-19 10:15:13 +0800

    fixed philly config bug

--- dff_dcn/train_end2end.py
+++ dff_dcn/train_end2end.py
@@ -161,8 +161,8 @@ def train_net(args, ctx, pretrained_dir, pretrained_resnet, pretrained_flow, epo
     means = np.tile(np.array(config.TRAIN.BBOX_MEANS), 2 if config.CLASS_AGNOSTIC else config.dataset.NUM_CLASSES)
     stds = np.tile(np.array(config.TRAIN.BBOX_STDS), 2 if config.CLASS_AGNOSTIC else config.dataset.NUM_CLASSES)
     epoch_end_callback = [mx.callback.module_checkpoint(mod, prefix, period=1, save_optimizer_states=True), callback.do_checkpoint(prefix, means, stds)]
-    batch_end_callback.append(mx.callback.module_checkpoint(mod, prefix, period=500, save_optimizer_states=True)) 
-    batch_end_callback.append(callback.do_checkpoint(prefix, means, stds, period=500))
+    # batch_end_callback.append(mx.callback.module_checkpoint(mod, prefix, period=500, save_optimizer_states=True)) 
+    # batch_end_callback.append(callback.do_checkpoint(prefix, means, stds, period=500))
     
     # decide learning rate
     base_lr = lr
--- dff_rfcn/config/config.py
+++ dff_rfcn/config/config.py
@@ -1,19 +1,15 @@
 # --------------------------------------------------------
 # Deep Feature Flow
-# Copyright (c) 2017 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Modified by Xizhou Zhu, Yuwen Xiong, Bin Xiao
-# --------------------------------------------------------
-# Based on:
-# MX-RCNN
 # Copyright (c) 2016 by Contributors
-# Licence under The Apache 2.0 License
-# https://github.com/ijkguo/mx-rcnn/
+# Copyright (c) 2017 Microsoft
+# Licensed under The Apache-2.0 License [see LICENSE for details]
+# Modified by Xizhou Zhu, Yuwen Xiong, Bin Xiao
 # --------------------------------------------------------
 
 import yaml
 import numpy as np
 from easydict import EasyDict as edict
+import os
 
 config = edict()
 
@@ -23,6 +19,7 @@ config.symbol = ''
 config.gpus = ''
 config.CLASS_AGNOSTIC = True
 config.SCALES = [(600, 1000)]  # first is scale (the shorter side); second is max size
+config.USE_PHILLY = False
 
 # default training
 config.default = edict()
@@ -31,7 +28,8 @@ config.default.kvstore = 'device'
 
 # network related params
 config.network = edict()
-config.network.pretrained = ''
+config.network.pretrained_dir = ''
+config.network.pretrained_resnet = ''
 config.network.pretrained_flow = ''
 config.network.pretrained_epoch = 0
 config.network.PIXEL_MEANS = np.array([0, 0, 0])
@@ -46,6 +44,9 @@ config.network.ANCHOR_MEANS = (0.0, 0.0, 0.0, 0.0)
 config.network.ANCHOR_STDS = (0.1, 0.1, 0.4, 0.4)
 config.network.NUM_ANCHORS = len(config.network.ANCHOR_SCALES) * len(config.network.ANCHOR_RATIOS)
 config.network.DFF_FEAT_DIM = 1024
+config.network.ROIDispatch = False
+config.network.USE_NONGT_INDEX = False
+config.network.NMS_TARGET_THRESH = '0.5'
 
 # dataset related params
 config.dataset = edict()
@@ -70,6 +71,9 @@ config.TRAIN.wd = 0.0005
 config.TRAIN.begin_epoch = 0
 config.TRAIN.end_epoch = 0
 config.TRAIN.model_prefix = ''
+config.TRAIN.rpn_loss_scale = 3.0
+config.TRAIN.nms_loss_scale = 1.0
+config.TRAIN.nms_pos_scale = 4.0
 
 # whether resume training
 config.TRAIN.RESUME = False
@@ -122,6 +126,10 @@ config.TRAIN.RPN_MIN_SIZE = config.network.RPN_FEAT_STRIDE
 config.TRAIN.BBOX_NORMALIZATION_PRECOMPUTED = True
 config.TRAIN.BBOX_MEANS = (0.0, 0.0, 0.0, 0.0)
 config.TRAIN.BBOX_STDS = (0.1, 0.1, 0.2, 0.2)
+# Learn NMS
+config.TRAIN.LEARN_NMS = False
+config.TRAIN.JOINT_TRAINING = False
+config.TRAIN.FIRST_N = 100
 
 # DFF, trained image sampled from [min_offset, max_offset]
 config.TRAIN.MIN_OFFSET = -9
@@ -141,6 +149,13 @@ config.TEST.RPN_NMS_THRESH = 0.7
 config.TEST.RPN_PRE_NMS_TOP_N = 6000
 config.TEST.RPN_POST_NMS_TOP_N = 300
 config.TEST.RPN_MIN_SIZE = config.network.RPN_FEAT_STRIDE
+
+# whether to use softnms
+config.TEST.SOFTNMS = False
+# whether to use LEARN_NMS
+config.TEST.LEARN_NMS = False
+config.TEST.FIRST_N = 0
+config.TEST.MERGE_METHOD = -1
 
 # RCNN nms
 config.TEST.NMS = 0.3
@@ -176,3 +191,16 @@ def update_config(config_file):
                         config[k] = v
             else:
                 raise ValueError("key must exist in config.py")
+
+def update_philly_config(model_dir, data_dir):
+    def _update_to_abs(prefix, basename):
+        if not os.path.isabs(basename):
+            print("Update {} with {}".format(basename, prefix))
+            return os.path.join(prefix, basename)
+        else:
+            return basename
+    
+    config.output_path = _update_to_abs(model_dir, config.output_path)
+    config.dataset.dataset_path = _update_to_abs(data_dir, config.dataset.dataset_path)
+    config.dataset.root_path = _update_to_abs(data_dir, config.dataset.root_path)
+    config.network.pretrained_dir = _update_to_abs(data_dir, config.network.pretrained_dir)
